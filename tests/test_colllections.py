1515c324bee75b415210f5366f1da6484500fcd7
Zain Quraishi <zainq.pro@gmail.com>
2023-05-17 14:47:57 -0400

    revert test file name and formatting

--- tests/test_colllections.py
+++ tests/test_colllections.py
@@ -21,8 +21,7 @@ def test_home_structure():
         ".DS_Store"
     ]
     current_directories = os.listdir()
-    correct_directories = [
-        x in expected_directories for x in current_directories]
+    correct_directories = [x in expected_directories for x in current_directories]
     assert all(correct_directories), 'Top level changes are not allowed'
 
 
@@ -31,8 +30,7 @@ def test_collections_structure():
     folders = [
         not os.path.isfile("{}/{}".format(COLLECTIONS, x)) for x in current_collections
     ]
-    assert all(
-        folders), 'Invalid structure, include your files in a nested directory'
+    assert all(folders), 'Invalid structure, include your files in a nested directory'
 
 
 def test_meta():
@@ -52,28 +50,23 @@ def test_meta():
         with open("{}/{}/meta.json".format(COLLECTIONS, x), "r") as file:
             meta = json.load(file)
 
-        assert set(meta.keys()) == set(
-            expected_meta.keys()), 'Invalid meta data keys'
+        assert set(meta.keys()) == set(expected_meta.keys()) , 'Invalid meta data keys'
 
         for y in zip(meta.values(), meta.keys()):
-            assert isinstance(y[0], str), 'Invalid data type, use a string'
-            if y[1].endswith('link'):
-                if y[0]:
-                    assert y[0].startswith(
-                        'https://') or y[0].startswith('http://'), 'link must start with https://'
+          assert isinstance(y[0], str) , 'Invalid data type, use a string'
+          if y[1].endswith('link'):
+            if y[0]:
+                assert y[0].startswith('https://') or y[0].startswith('http://'), 'link must start with https://'
 
-        assert (len(meta.get('inscription_icon')) == 66) or meta.get(
-            'inscription_icon'), 'Invalid inscription Id'
-        assert meta.get('slug').lower() == meta.get(
-            'slug'), 'Slug must be lowercase'
+        assert (len(meta.get('inscription_icon')) == 66) or meta.get('inscription_icon'), 'Invalid inscription Id'
+        assert meta.get('slug').lower() == meta.get('slug'), 'Slug must be lowercase'
         assert len(meta.get('name')) <= 60, 'Name is too long'
         assert len(meta.get('slug')) < 60, 'Slug is too long'
         assert meta.get('slug') == x, 'Slug does not match directory name'
 
-
 def ishex(s):
     try:
-        n = int(s, 16)
+        n = int(s,16)
         return True
     except ValueError:
         return False
@@ -85,20 +78,18 @@ def test_inscriptions():
         with open("{}/{}/inscriptions.json".format(COLLECTIONS, x), "r") as file:
             inscriptions = json.load(file)
         for y in inscriptions:
-            assert y.get('id')
-            assert y.get('meta')
-            assert y.get(
-                'attributes') is None, 'Attributes belong in meta object'
-            if y.get('meta').get('attributes'):
-                for a in y.get('meta').get('attributes'):
-                    if x not in ['ordinal-gen1-pokemon', 'bitcoin-jpgs']:
-                        assert 'trait_type' in a, 'Attribute must have trait type'
-                        assert 'value' in a, 'Attribute must have trait value'
-            assert len(y.get('id').strip()) == 66
-            assert ishex(y.get('id')[0:64]
-                         ), 'inscription ids must be valid hex'
-            assert isinstance(y.get('meta').get('name'), str)
-
+          assert y.get('id')
+          assert y.get('meta')
+          assert y.get('attributes') is None, 'Attributes belong in meta object'
+          if y.get('meta').get('attributes'):
+             for a in y.get('meta').get('attributes'):
+               if x not in ['ordinal-gen1-pokemon', 'bitcoin-jpgs']:
+                assert 'trait_type' in a, 'Attribute must have trait type'
+                assert 'value' in a, 'Attribute must have trait value'
+          assert len(y.get('id').strip()) == 66
+          assert ishex(y.get('id')[0:64]), 'inscription ids must be valid hex'
+          assert isinstance(y.get('meta').get('name'), str)
+         
 
 def test_uniqueness():
     input_collections = os.listdir(COLLECTIONS)
@@ -107,11 +98,12 @@ def test_uniqueness():
     # add new collections
     all_inscription_ids = []
     for collection in input_collections:
-        with open("{}/{}/inscriptions.json".format(COLLECTIONS, collection), "r") as file:
-            inscriptions = json.load(file)
-        inscription_ids = []
-        for x in inscriptions:
-            inscription_ids.append(x.get('id'))
-        all_inscription_ids = all_inscription_ids + inscription_ids
-        duplicates = len(all_inscription_ids) - len(set(all_inscription_ids))
-        assert duplicates == 0
+      with open("{}/{}/inscriptions.json".format(COLLECTIONS, collection), "r") as file:
+        inscriptions = json.load(file)
+      inscription_ids = []
+      for x in inscriptions:
+        inscription_ids.append(x.get('id'))
+      all_inscription_ids = all_inscription_ids + inscription_ids
+      duplicates = len(all_inscription_ids) - len(set(all_inscription_ids))
+      assert duplicates == 0
+      
