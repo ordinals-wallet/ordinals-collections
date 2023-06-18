00015c01726dacbe67b4054d0e17b10c18590f30
nantekottai5 <136910646+nantekottai5@users.noreply.github.com>
2023-06-18 19:29:05 +0900

    Update test_collections.py

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -62,7 +62,7 @@ def test_inscriptions():
     current_collections = os.listdir(COLLECTIONS)
     for x in current_collections:
         with open("{}/{}/inscriptions.json".format(COLLECTIONS, x), "r") as file:
-with open("path/to/collections/example_collection/meta.json", "r") as file:
+    with open("path/to/collections/example_collection/meta.json", "r") as file:
         # ファイルの読み込み処理
         inscriptions = json.load(file)
         for y in inscriptions:
