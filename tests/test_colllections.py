84cc341e2bf1a188c730a81dc1c87d3f0df56a61
nantekottai5 <136910646+nantekottai5@users.noreply.github.com>
2023-06-18 19:03:55 +0900

    Update test_colllections.py

--- tests/test_colllections.py
+++ tests/test_colllections.py
@@ -1,7 +1,6 @@
+
 import os
 import json
-import pytest
-
 COLLECTIONS = "./collections"
 def test_home_structure():
     expected_directories = [
@@ -25,10 +24,9 @@ def test_home_structure():
 def test_collections_structure():
     current_collections = os.listdir(COLLECTIONS)
     folders = [
-        not os.path.isfile(f"{COLLECTIONS}/{x}") for x in current_collections
+        not os.path.isfile("{}/{}".format(COLLECTIONS, x)) for x in current_collections
     ]
     assert all(folders), 'Invalid structure, include your files in a nested directory'
-
 def test_meta():
     expected_meta = {
         "name": "Based Apes",
@@ -41,24 +39,20 @@ def test_meta():
         "website_link": "",
     }
     current_collections = os.listdir(COLLECTIONS)
-
     for x in current_collections:
-        with open(f"{COLLECTIONS}/{x}/meta.json", "r") as file:
-            try:
-                meta = json.load(file)
-            except json.JSONDecodeError as e:
-                pytest.fail(f"Failed to decode JSON in {x}/meta.json: {str(e)}")
-
-            assert set(meta.keys()) == set(expected_meta.keys()), 'Invalid meta data keys'
-
-            for y in zip(meta.values(), meta.keys()):
-                assert isinstance(y[0], str), 'Invalid data type, use a string'
-                if y[1].endswith('link'):
-                    if y[0]:
-                        assert y[0].startswith('https://') or y[0].startswith('http://'), 'link must start with https://'
-
-            assert len(meta.get('inscription_icon', '')) == 66 or meta.get('inscription_icon')
-
+with open("{nantekottai5}/{nidomichan}/meta.json".format(COLLECTIONS, x), "r") as file:
+    meta = json.load(file)
+        assert set(meta.keys()) == set(expected_meta.keys()) , 'Invalid meta data keys'
+        for y in zip(meta.values(), meta.keys()):
+          assert isinstance(y[0], str) , 'Invalid data type, use a string'
+          if y[1].endswith('link'):
+            if y[0]:
+                assert y[0].startswith('https://') or y[0].startswith('http://'), 'link must start with https://'
+        assert (len(meta.get('inscription_icon')) == 66) or meta.get('inscription_icon'), 'Invalid inscription Id'
+        assert meta.get('slug').lower() == meta.get('slug'), 'Slug must be lowercase'
+        assert len(meta.get('name')) <= 60, 'Name is too long'
+        assert len(meta.get('slug')) < 60, 'Slug is too long'
+        assert meta.get('slug') == x, 'Slug does not match directory name'
 def ishex(s):
     try:
         n = int(s,16)
@@ -67,41 +61,31 @@ def ishex(s):
         return False
 def test_inscriptions():
     current_collections = os.listdir(COLLECTIONS)
-
     for x in current_collections:
-        with open(f"{COLLECTIONS}/{x}/meta.json", "r") as file:
-            meta = json.load(file)
-
-        # Fetch inscriptions from the meta object
-        inscriptions = meta.get('inscriptions', [])
+with open("{}/{}/meta.json".format(COLLECTIONS, x), "r") as file:
+    # ファイルの読み込み処理
+            inscriptions = json.load(file)
+with open("{}/{{}}/meta.json".format(COLLECTIONS).format(x), "r") as file:
+    meta = json.load(file)
 
         for y in inscriptions:
-            # Add assertions for inscriptions
-
+          assert y.get('id')
+          assert y.get('meta')
+          assert y.get('attributes') is None, 'Attributes belong in meta object'
+          if y.get('meta').get('attributes'):
+             for a in y.get('meta').get('attributes'):
+               if x not in ['ordinal-gen1-pokemon', 'bitcoin-jpgs']:
+                assert 'trait_type' in a, 'Attribute must have trait type'
+                assert 'value' in a, 'Attribute must have trait value'
+          assert len(y.get('id').strip()) == 66
+          assert ishex(y.get('id')[0:64]), 'inscription ids must be valid hex'
+          assert isinstance(y.get('meta').get('name'), str)
+         
 def test_uniqueness():
     input_collections = os.listdir(COLLECTIONS)
     print('\n\n')
-
-    # Add new collections
+    # add new collections
     all_inscription_ids = []
     for collection in input_collections:
-        with open(f"{COLLECTIONS}/{collection}/inscriptions.json", "r") as file:
-            try:
-                inscriptions = json.load(file)
-            except json.JSONDecodeError as e:
-                pytest.fail(f"Failed to decode JSON in {collection}/inscriptions.json: {str(e)}")
-
-            # Assert uniqueness of inscription IDs
-            for inscription in inscriptions:
-                inscription_id = inscription.get("id")
-                assert inscription_id, "Inscription ID is missing"
-                assert inscription_id not in all_inscription_ids, f"Duplicated inscription ID: {inscription_id}"
-                all_inscription_ids.append(inscription_id)
-
-            # Assert other conditions for inscriptions
-
-            # Rest of the assertions
-
-            # Add other assertions for inscriptions
-
-            # Rest of the assertions
+      with open("{}/{}/inscriptions.json".format(COLLECTIONS, collection), "r") as file:
+        inscriptions = json.load(file)
